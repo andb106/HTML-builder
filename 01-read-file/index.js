88eb08312a424cc96041dd3170d7fc484f97b93e
Andrey Boytsov <po4ta_notice@mail.ru>
2022-10-30 23:09:03 +0300

    feat: add solution for task 1

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs');
+const { stdout } = require('process');
+
+const pathToFile = path.join(__dirname, 'text.txt');
+const myReadStream = fs.createReadStream(pathToFile, 'utf-8');
+
+myReadStream.on('data', (data) => stdout.write(data));
