d865b1fb64608e31cb29b97944ee635869eb78ac
Andrey Boytsov <po4ta_notice@mail.ru>
2022-11-02 22:56:09 +0300

    feat: add solutions for tasks 4, 5

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const { readdir, mkdir, rm, copyFile } = require('fs/promises');
+const path = require('path');
 
+const pathToCopyFolder = path.join(__dirname, './files-copy');
+const pathToFolder = path.join(__dirname, '/files');
+
+async function copyDir(pathSrc, pathDest) {
+  await rm(pathDest, {recursive: true, force: true});
+  await mkdir(pathDest, {recursive: true});
+
+  const items = await readdir(pathSrc, {withFileTypes: true});
+  for (let item of items) {
+    const itemSrcPath = path.join(pathSrc, item.name);
+    const itemDestPath = path.join(pathDest, item.name);
+    await copyFile(itemSrcPath, itemDestPath);
+  }
+}
+
+copyDir(pathToFolder, pathToCopyFolder);
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const { readdir} = require('fs/promises');
+const path = require('path');
+const fs = require('fs');
+
+const pathToFolder = path.join(__dirname, '/styles');
+const pathToOutputFile = path.join(__dirname, '/project-dist','bundle.css');
+
+// const pathToFolder = path.join(__dirname, '/test-files', '/styles');
+// const pathToOutputFile = path.join(__dirname, '/test-files','bundle.css');
+
+async function bundleCss (pathToSomeFolder, pathToOutput) {
+  const ws = fs.createWriteStream(pathToOutput);
+  try {
+    const files = await readdir(pathToSomeFolder, {withFileTypes: true});
+    for (const file of files) {
+      if (file.isFile() && path.extname(file.name) === '.css') {
+        console.log(file.name);
+        const pathToFile = path.join(pathToSomeFolder, file.name);
+        const rs = fs.createReadStream(pathToFile, 'utf-8');
+        rs.on('data', (data) => ws.write(data));
+      }
+    };
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+bundleCss(pathToFolder, pathToOutputFile);
