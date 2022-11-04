cee07a98ad2150f1c28f4962cd859bee93422ef3
Andrey Boytsov <po4ta_notice@mail.ru>
2022-11-04 23:44:23 +0300

    refactor: delete console.log and comments in task5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,8 +5,6 @@ const fs = require('fs');
 const pathToFolder = path.join(__dirname, '/styles');
 const pathToOutputFile = path.join(__dirname, '/project-dist','bundle.css');
 
-// const pathToFolder = path.join(__dirname, '/test-files', '/styles');
-// const pathToOutputFile = path.join(__dirname, '/test-files','bundle.css');
 
 async function bundleCss (pathToSomeFolder, pathToOutput) {
   const ws = fs.createWriteStream(pathToOutput);
@@ -14,7 +12,6 @@ async function bundleCss (pathToSomeFolder, pathToOutput) {
     const files = await readdir(pathToSomeFolder, {withFileTypes: true});
     for (const file of files) {
       if (file.isFile() && path.extname(file.name) === '.css') {
-        console.log(file.name);
         const pathToFile = path.join(pathToSomeFolder, file.name);
         const rs = fs.createReadStream(pathToFile, 'utf-8');
         rs.on('data', (data) => ws.write(data));
